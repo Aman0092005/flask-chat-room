a2a1dbc47a0247134bdedcc0a3f1f267d4c0274d
Aman0092005 <75556045+Aman0092005@users.noreply.github.com>
2025-05-29 17:20:27 +0530

    Update main.py for deployment

--- main.py
+++ main.py
@@ -105,4 +105,5 @@ def disconnect():
 
 if __name__ == "__main__":
     #socketio.run(app,debug=True, host="0.0.0.0", port=5000)
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True) # for deployment
+    if __name__ == "__main__":
+    socketio.run(app, debug=True, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True) # for deployment
