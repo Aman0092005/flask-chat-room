faa1cbc8f13c682a5e981a7ee3602b6b70422eaa
Aman0092005 <75556045+Aman0092005@users.noreply.github.com>
2025-05-29 17:22:43 +0530

    Update main.py solve indentation issue in running

--- main.py
+++ main.py
@@ -106,4 +106,4 @@ def disconnect():
 if __name__ == "__main__":
     #socketio.run(app,debug=True, host="0.0.0.0", port=5000)
     if __name__ == "__main__":
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True) # for deployment
+        socketio.run(app, debug=True, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True) # for deployment
